ca79073d03ba065e426db0716c103788a4373491
Zalthalion <Freidenfeldsjuirs@gmail.com>
2020-06-12 07:11:26 +0200

    Final push
    Final push before presentation

--- GUI.py
+++ GUI.py
@@ -1,7 +1,7 @@
 # GUI.py
 # RUN THIS FILE
 import pygame
-from somethung import solve, valid
+from somethung import valid
 import time
 pygame.font.init()
 #Global variable, that shows if the user has given up
--- Main.py
+++ Main.py
@@ -3,7 +3,20 @@ from Reader import Reader   #Imports the sudoku reader part
 from GUI import Game        #Imports the sudoku game part
 
 readingPart = Reader()
-puzzleArray = readingPart.Read('sudoku-puzzle-games.jpg')
+
+# puzzleArray = readingPart.Read('23615025664_ea6867718f_b.jpg')
+# puzzleArray = readingPart.Read('image1.jpg')
+# puzzleArray = readingPart.Read('sudoku-puzzle-games.jpg')
+# puzzleArray = readingPart.Read('Untitled-1_2260717b.jpg')
+puzzleArray = readingPart.Read('sudoku 001.jpg')
+
+
+
+
+
+
+
+
 
 #Gets a 2D list with the sudoku grid (not solwed)
 solvingPart = Solver()
